fa436e30f1ec21cb7d9b07fe66eebbe6a9110b5c
Pg Gonni <59372630+sekeidesign@users.noreply.github.com>
2024-12-19 07:22:44 -0500

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Piergiorgio Gonni on 2024-12-19.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
